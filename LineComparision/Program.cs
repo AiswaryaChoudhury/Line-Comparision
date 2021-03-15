075820ff47e4ccdab64527137d12629e57965236
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-15 09:19:12 +0530

    [AiswaryaChoudhury] Add : Calculate length of Co-ordinates feature Implemented

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -7,6 +7,15 @@ namespace LineComparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation Program");
+            int x1 = Convert.ToInt32(Console.ReadLine());                
+            int x2 = Convert.ToInt32(Console.ReadLine()); 
+            int y1 = Convert.ToInt32(Console.ReadLine()); 
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Sqrt(Math.Pow(x2 - x1, 2) +
+                      Math.Pow(y2 - y1, 2)));
+
+
         }
     }
 }
