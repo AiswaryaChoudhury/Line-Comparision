0478b240864d5a35fff4039c5175ef65be71b8f8
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-16 17:48:55 +0530

    [AiswaryaChoudhury] Add : Project Structure Created

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -5,19 +5,8 @@ namespace LineComparision
     class Program
     {
         static void Main(string[] args)
-        {
-<<<<<<< HEAD
+        { 
             Console.WriteLine("Welcome To Line Comparision Computation Program");
-=======
-            Console.WriteLine("Welcome to Line Comparision Computation Program");
-            int startpoint1 = Convert.ToInt32(Console.ReadLine());                
-            int endpoint1 = Convert.ToInt32(Console.ReadLine()); 
-            int startpoint2 = Convert.ToInt32(Console.ReadLine());
-            int endpoint2 = Convert.ToInt32(Console.ReadLine());
-            double length;
-            length = Math.Sqrt(Math.Pow(endpoint1 - startpoint1, 2) + Math.Pow(endpoint2 - startpoint2, 2));
-            Console.WriteLine("Length of two Co-ordinates :" + length);
->>>>>>> UC1-CalculateLength
         }
     }
 }
