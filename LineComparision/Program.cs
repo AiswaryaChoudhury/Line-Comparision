7c05b68b13f3e8dfe82a041b2b10ab3fb0c13057
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-17 17:53:49 +0530

    [AiswaryaChoudhury] Add : Compare two Lines Using CompareTo Feature Implemented

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -14,13 +14,8 @@ namespace LineComparision
             double lengthLine1, lengthLine2;
             lengthLine1= Math.Sqrt(Math.Pow(line1.endpoint1 - line1.startpoint1, 2) + Math.Pow(line1.endpoint2 - line1.startpoint2, 2));       
             lengthLine2= Math.Sqrt(Math.Pow(line2.endpoint1 - line2.startpoint1, 2) + Math.Pow(line2.endpoint2 - line2.startpoint2, 2));
-            if (lengthLine1.Equals(lengthLine2))
-                Console.WriteLine("Length of two lines are equal");
-            if (lengthLine1 > lengthLine2)
-                Console.WriteLine("Length of line 1 is greater than Line 2");
-            else
-                Console.WriteLine("Length of two lines are not equal");
-
+            Console.WriteLine("Length of two lines are equal is "+ lengthLine1.Equals(lengthLine2));
+            Console.WriteLine("Comparision of lines :"+ lengthLine1.CompareTo(lengthLine2));
         }
     }
 }
