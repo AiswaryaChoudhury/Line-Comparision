f7a8ff246245dc067ad56af2920f47b44387d821
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-17 07:06:00 +0530

    [AiswaryaChoudhury] Add : Compare two Lines Feature Implemented

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -4,20 +4,12 @@ namespace LineComparision
 {
     class Program
     {
-        int startpoint1, endpoint1, startpoint2, endpoint2;
-        public Program()
-        {
-            startpoint1 = Convert.ToInt32(Console.ReadLine());
-            endpoint1 = Convert.ToInt32(Console.ReadLine());
-            startpoint2 = Convert.ToInt32(Console.ReadLine());
-            endpoint2 = Convert.ToInt32(Console.ReadLine());
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the Co -ordinates of 1st line:");
-            Program line1 = new Program();
+            Length line1 = new Length();
             Console.WriteLine("Enter the Co -ordinates of 2nd line:");
-            Program line2 = new Program();
+            Length line2 = new Length();
             double lengthLine1, lengthLine2;
             lengthLine1 = Math.Sqrt(Math.Pow(line1.endpoint1 - line1.startpoint1, 2) + Math.Pow(line1.endpoint2 - line1.startpoint2, 2));
             lengthLine2 = Math.Sqrt(Math.Pow(line2.endpoint1 - line2.startpoint1, 2) + Math.Pow(line2.endpoint2 - line2.startpoint2, 2));
